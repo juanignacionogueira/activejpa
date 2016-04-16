cdc1c18f8de5e88f09c66c3c91f6bdd3003c8cc3
jnogueira <juanignacionogueira@gmail.com>
2016-04-16 09:14:58 -0300

    Java 8 Upgrade. AgentLoader Windows fix.

--- activejpa-core/src/main/java/org/activejpa/enhancer/ActiveJpaAgentLoader.java
+++ activejpa-core/src/main/java/org/activejpa/enhancer/ActiveJpaAgentLoader.java
@@ -3,12 +3,17 @@
  */
 package org.activejpa.enhancer;
 
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
+import java.net.URISyntaxException;
 import java.security.CodeSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
 
 /**
@@ -18,29 +23,35 @@ import com.sun.tools.attach.VirtualMachine;
 public class ActiveJpaAgentLoader {
 
 	private static final Logger logger = LoggerFactory.getLogger(ActiveJpaAgentLoader.class);
-	
+
 	private static final ActiveJpaAgentLoader loader = new ActiveJpaAgentLoader();
-	
+
 	private ActiveJpaAgentLoader() {
 	}
-	
+
 	public static ActiveJpaAgentLoader instance() {
 		return loader;
 	}
 
-    public void loadAgent() {
-        logger.info("dynamically loading javaagent");
-        String nameOfRunningVM = ManagementFactory.getRuntimeMXBean().getName();
-        int p = nameOfRunningVM.indexOf('@');
-        String pid = nameOfRunningVM.substring(0, p);
-        
-        try {
-            VirtualMachine vm = VirtualMachine.attach(pid);
-            CodeSource codeSource = ActiveJpaAgent.class.getProtectionDomain().getCodeSource();
-            vm.loadAgent(codeSource.getLocation().toURI().getPath(), "");
-            vm.detach();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public void loadAgent() throws RuntimeException {
+		logger.info("dynamically loading javaagent");
+		String nameOfRunningVM = ManagementFactory.getRuntimeMXBean().getName();
+		int p = nameOfRunningVM.indexOf('@');
+		String pid = nameOfRunningVM.substring(0, p);
+
+		try {
+			VirtualMachine vm = VirtualMachine.attach(pid);
+			CodeSource codeSource = ActiveJpaAgent.class.getProtectionDomain().getCodeSource();
+			String OS = System.getProperty("os.name");
+			String path = codeSource.getLocation().toURI().getPath();
+			if (path != null && path.startsWith("/") && OS.startsWith("Windows")) {
+				path = path.substring(1);
+			}
+			vm.loadAgent(path, "");
+			vm.detach();
+		} catch (AttachNotSupportedException | IOException | URISyntaxException | AgentLoadException
+				| AgentInitializationException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
